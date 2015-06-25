45c941395885d79b5957dffd4f0c92c58f9691c5
Kolp96 <mateusz.pysz.18@gmail.com>
2015-06-25 20:26:21 +0200

    Broadcast
    to co się wyświetla  przy logowaniu

--- data/globalevents/scripts/anuncio.lua
+++ data/globalevents/scripts/anuncio.lua
@@ -1,7 +1,6 @@
--- [( Script created by Matheus for TibiaKing.com )] --
 function onThink(interval, lastExecution)
 MENSAGEM = {
-"Zapraszamy na storne http://shinobiartonline.eu/ ",
+"Visit our website shinobiartonline.eu",
 }
 doBroadcastMessage(MENSAGEM[math.random(1,#MENSAGEM)],22)
 return TRUE
